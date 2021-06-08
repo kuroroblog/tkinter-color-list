3159e10a5b1433e559f3c9ae7099cdf55210e24f
tocchimittsu <arimagedon21@gmail.com>
2021-06-08 13:44:58 +0900

    修正

---
+++ main.py
@@ -0,0 +1,73 @@
+import tkinter as tk
+from tkinter import colorchooser
+
+
+class Application(tk.Frame):
+    # 色を確認するための関数
+    def checkColor(self):
+        colorchooser.askcolor()
+
+    # 色に対して名前を指定して、label Widgetを表示する関数
+    def getColorNameLabel(self):
+        # label Widgetを作成する。
+        # text : テキスト情報
+        # width : 幅の設定
+        # height : 高さの設定
+        # bg : 背景色の設定
+        # 色の設定を変更する場合 : http://www.tcl.tk/man/tcl/TkCmd/colors.htm
+        # label Widgetについて : https://kuroro.blog/python/Pj4Z7JBNRvcHZvtFqiKD/
+        label = tk.Label(text="test", width=30, height=15, bg="green")
+        # Windowを親要素とした場合に、label Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        label.pack()
+
+    # 色に対してRGBを指定して、label Widgetを表示する関数
+    # RGBについて : https://digitalidentity.co.jp/blog/creative/about-rgb-cmyk.html
+    def getRGBLabel(self):
+        # label Widgetを作成する。
+        # text : テキスト情報
+        # width : 幅の設定
+        # height : 高さの設定
+        # bg : 背景色の設定
+        # RGBを変更する場合 : http://www.netyasun.com/home/color.html
+        # label Widgetについて : https://kuroro.blog/python/Pj4Z7JBNRvcHZvtFqiKD/
+        label = tk.Label(text="test", width=30, height=15, bg="#008000")
+        # Windowを親要素とした場合に、label Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        label.pack()
+
+    # label Widgetを表示する関数
+    def getLabel(self):
+        # label Widgetを作成する。
+        # text : テキスト情報
+        # width : 幅の設定
+        # height : 高さの設定
+        # bg : 背景色の設定
+        # 色の設定を変更する場合 : http://www.tcl.tk/man/tcl/TkCmd/colors.htm
+        # label Widgetについて : https://kuroro.blog/python/Pj4Z7JBNRvcHZvtFqiKD/
+        label = tk.Label(text="test", width=30, height=15, bg="red")
+        # Windowを親要素とした場合に、label Widgetをどのように配置するのか?
+        # packについて : https://kuroro.blog/python/UuvLfIBIEaw98BzBZ3FJ/
+        label.pack()
+
+    def __init__(self, master=None):
+        # Windowの初期設定を行う。
+        super().__init__(master)
+        # Windowの画面サイズを設定する。
+        # geometryについて : https://kuroro.blog/python/rozH3S2CYE0a0nB3s2QL/
+        self.master.geometry("300x200")
+
+        # self.getLabel()
+        # self.getRGBLabel()
+        # self.getColorNameLabel()
+        # self.checkColor()
+
+
+# Tkinter初学者参考 : https://docs.python.org/ja/3/library/tkinter.html#a-simple-hello-world-program
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(master=root)
+
+    # Windowをループさせて、継続的にWindow表示させる。
+    # mainloopについて : https://kuroro.blog/python/DmJdUb50oAhmBteRa4fi/
+    app.mainloop()
